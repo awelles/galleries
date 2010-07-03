ea71a6224c4c834e7a0193316d16a629c827f8c9
Aaron Welles <aaron@welles.net>
2010-07-03 16:54:22 -0400

    added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :groups
-
   map.resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
