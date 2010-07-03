aa03799fc66eeb30608ab69210d322e93ed2d804
Aaron Welles <aaron@welles.net>
2010-07-03 02:04:31 -0400

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100703012806) do
+ActiveRecord::Schema.define(:version => 20100703055433) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
