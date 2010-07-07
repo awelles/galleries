70edc8a01b14fd9c8dfe33c9cd5ace41bcf7220a
Aaron Welles <aaron@welles.net>
2010-07-07 16:29:22 -0400

    working on pages view

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,31 +1,45 @@
 require 'spec_helper'
 
 describe PagesController do
-
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
-  end
-
-
+  integrate_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
+	
+	it "should have the right title" do
+	  get 'home'
+	  response.should have_tag("title",
+							   "Galleries | Home")
+	end # "should have the right title
+  end # GET home
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
-  end
+	
+	it "should have the right title" do
+	  get 'contact'
+	  response.should have_tag("title",
+							   "Galleries | Contact")
+	end # "should have the right title
+  end # GET contact
   
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-  end
+	
+	it "should have the right title" do
+	  get 'about'
+	  response.should have_tag("title",
+							   "Galleries | Home")
+	end # "should have the right title
+  end # GET about
+  
 end
